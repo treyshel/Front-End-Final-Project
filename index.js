bded32f42c108f2269ab84245d669aa19b1fee97
Trey Shelton <tshelton@basecampcodingacademy.org>
2017-11-27 12:56:21 -0600

    all of my work i've done.. im almost done and just realized that all my commits go to nate's repository that i cloned from

---
+++ index.js
@@ -0,0 +1,598 @@
+var CLOTHING = {
+    outerwear: [
+        {
+            jackets: {
+                description: 'Barn Coat',
+                price: 169.99,
+                stock: 10,
+                image: 'photos/jacket_1.jpg'
+            }
+        },
+        {
+            jackets: {
+                description: 'Weatherbreaker Jacket',
+                price: 169.99,
+                stock: 10,
+                image: 'photos/jacket_2.jpg'
+            }
+        },
+        {
+            jackets: {
+                description: 'Barbour Ashby Jacket',
+                price: 399.99,
+                stock: 10,
+                image: 'photos/jacket_3.jpg'
+            }
+        },
+        {
+            jackets: {
+                description: 'Heritage Field Coat',
+                price: 249.99,
+                stock: 10,
+                image: 'photos/jacket_4.jpg'
+            }
+        },
+        {
+            jackets: {
+                description: 'Bedale Jacket',
+                price: 379.99,
+                stock: 10,
+                image: 'photos/jacket_5.jpg'
+            }
+        },
+        {
+            jackets: {
+                description: 'Brome Jacket',
+                price: 429.99,
+                stock: 10,
+                image: 'photos/jacket_6.jpg'
+            }
+        },
+        {
+            jackets: {
+                description: 'Wool Driving Coat',
+                price: 298.99,
+                stock: 10,
+                image: 'photos/jacket_7.jpg'
+            }
+        },
+        {
+            jackets: {
+                description: 'Bedale Jacket',
+                price: 379.99,
+                stock: 10,
+                image: 'photos/jacket_8.jpg'
+            }
+        },
+        {
+            jackets: {
+                description: 'Shearling Coat',
+                price: 999.99,
+                stock: 10,
+                image: 'photos/jacket_9.jpg'
+            }
+        },
+        {
+            jackets: {
+                description: 'Redfield Leather Jacket',
+                price: 494.99,
+                stock: 10,
+                image: 'photos/jacket_10.jpg'
+            }
+        }
+    ],
+    collaredshirts: [
+        {
+            collared: {
+                description: 'Brushed Herringbone',
+                price: 89.99,
+                stock: 10,
+                image: 'photos/collared_1.jpg'
+            }
+        },
+        {
+            collared: {
+                description: 'Tartan Long-Sleeved Shirt',
+                price: 89.99,
+                stock: 10,
+                image: 'photos/collared_2.jpg'
+            }
+        },
+        {
+            collared: {
+                description: 'Signature Twill Long-Sleeved Shirt',
+                price: 89.99,
+                stock: 10,
+                image: 'photos/collared_3.jpg'
+            }
+        },
+        {
+            collared: {
+                description: 'Pure Cotton Denim Shirt',
+                price: 89.99,
+                stock: 10,
+                image: 'photos/collared_4.jpg'
+            }
+        },
+        {
+            collared: {
+                description:
+                    'Hidden-Button-Down Wrinkle-Free Cotton Twill Shirt',
+                price: 89.99,
+                stock: 10,
+                image: 'photos/collared_5.jpg'
+            }
+        },
+        {
+            collared: {
+                description: 'Heathered Houndstooth Shirt',
+                price: 89.99,
+                stock: 10,
+                image: 'photos/collared_6.jpg'
+            }
+        },
+        {
+            collared: {
+                description: 'Tech Chambray Work Shirt',
+                price: 89.99,
+                stock: 10,
+                image: 'photos/collared_7.jpg'
+            }
+        },
+        {
+            collared: {
+                description: 'Luxury Flannel Shirt',
+                price: 89.99,
+                stock: 10,
+                image: 'photos/collared_8.jpg'
+            }
+        },
+        {
+            collared: {
+                description: 'Bush Shirt',
+                price: 89.99,
+                stock: 10,
+                image: 'photos/collared_9.jpg'
+            }
+        },
+        {
+            collared: {
+                description: 'Corded Long-Sleeved Shirt',
+                price: 89.99,
+                stock: 10,
+                image: 'photos/collared_10.jpg'
+            }
+        }
+    ],
+    tshirts: [
+        {
+            tshirt: {
+                description: 'Dri-Release Casting T-Shirt',
+                price: 49.99,
+                stock: 10,
+                image: 'photos/tshirt_1.jpg'
+            }
+        },
+        {
+            tshirt: {
+                description: 'Dog Stamp Tee',
+                price: 49.99,
+                stock: 10,
+                image: 'photos/tshirt_2.jpg'
+            }
+        },
+        {
+            tshirt: {
+                description: 'Trout Stamp Tee',
+                price: 49.99,
+                stock: 10,
+                image: 'photos/tshirt_3.jpg'
+            }
+        },
+        {
+            tshirt: {
+                description: 'Hawker Henley',
+                price: 79.99,
+                stock: 10,
+                image: 'photos/tshirt_4.jpg'
+            }
+        },
+        {
+            tshirt: {
+                description: 'Montana Morning Retro',
+                price: 69.99,
+                stock: 10,
+                image: 'photos/tshirt_5.jpg'
+            }
+        },
+        {
+            tshirt: {
+                description: 'Pima Cotton Mockneck',
+                price: 89.99,
+                stock: 10,
+                image: 'photos/tshirt_6.jpg'
+            }
+        },
+        {
+            tshirt: {
+                description: 'Skyline Long-Sleeved',
+                price: 49.99,
+                stock: 10,
+                image: 'photos/tshirt_7.jpg'
+            }
+        },
+        {
+            tshirt: {
+                description: 'Rooster Long-Sleeved',
+                price: 49.99,
+                stock: 10,
+                image: 'photos/tshirt_8.jpg'
+            }
+        },
+        {
+            tshirt: {
+                description: 'Brown Trout Dri-Release Long-Sleeved',
+                price: 49.99,
+                stock: 10,
+                image: 'photos/tshirt_9.jpg'
+            }
+        },
+        {
+            tshirt: {
+                description: 'Fish Dri-Release Long-Sleeved',
+                price: 49.99,
+                stock: 10,
+                image: 'photos/tshirt_10.jpg'
+            }
+        }
+    ],
+    pants: [
+        {
+            khakis: {
+                description: 'County Donegal Tweed Pants',
+                price: 169.99,
+                stock: 10,
+                image: 'photos/pants_1.jpg'
+            }
+        },
+        {
+            khakis: {
+                description: 'Ultimate Khakis-Plain',
+                price: 119.99,
+                stock: 10,
+                image: 'photos/pants_2.jpg'
+            }
+        },
+        {
+            khakis: {
+                description: 'Supercord Pants',
+                price: 119.99,
+                stock: 10,
+                image: 'photos/pants_3.jpg'
+            }
+        },
+        {
+            khakis: {
+                description: 'Comfort-Waist Merino Flannel Pants',
+                price: 149.99,
+                stock: 10,
+                image: 'photos/pants_4.jpg'
+            }
+        },
+        {
+            khakis: {
+                description: '1856 Stretch Cords',
+                price: 109.99,
+                stock: 10,
+                image: 'photos/pants_5.jpg'
+            },
+            khakis: {
+                description: 'Stretch Moleskin Pants Plain',
+                price: 149.99,
+                stock: 10,
+                image: 'photos/pants_6.jpg'
+            }
+        },
+        {
+            khakis: {
+                description: 'Wrinkle-Free Pure Cotton Chinos',
+                price: 98.99,
+                stock: 10,
+                image: 'photos/pants_7.jpg'
+            }
+        },
+        {
+            khakis: {
+                description: 'Wrinkle-Free Comfort-Waist Chinos',
+                price: 109.99,
+                stock: 10,
+                image: 'photos/pants_8.jpg'
+            }
+        },
+        {
+            khakis: {
+                description: 'Insulated Rider Pants',
+                price: 79.99,
+                stock: 10,
+                image: 'photos/pants_9.jpg'
+            }
+        },
+        {
+            khakis: {
+                description: 'All Transit Pants',
+                price: 98.99,
+                stock: 10,
+                image: 'photos/pants_10.jpg'
+            }
+        }
+    ],
+    sockfootwear: [
+        {
+            socks: {
+                description: 'Invincible Socks',
+                price: 49.99,
+                stock: 10,
+                image: 'photos/socks_1.jpg'
+            }
+        },
+        {
+            socks: {
+                description: 'Invincible Cotton Sock',
+                price: 39.99,
+                stock: 10,
+                image: 'photos/socks_2.jpg'
+            }
+        },
+        {
+            socks: {
+                description: 'Isle Socks',
+                price: 39.99,
+                stock: 10,
+                image: 'photos/socks_3.jpg'
+            }
+        },
+        {
+            socks: {
+                description: 'Invincible Athletic Socks',
+                price: 39.99,
+                stock: 10,
+                image: 'photos/socks_4.jpg'
+            }
+        }
+    ],
+    shoefootwear: [
+        {
+            shoes: {
+                description: 'Australian Shearling-Lined Slippers',
+                price: 199.99,
+                stock: 10,
+                image: 'photos/shoes_1.jpg'
+            }
+        },
+        {
+            shoes: {
+                description: 'Gokey Shearling Lined Double-Sole Mocs',
+                price: 229.99,
+                stock: 10,
+                image: 'photos/shoes_2.jpg'
+            }
+        },
+        {
+            shoes: {
+                description: 'Ultralight 8" Boots',
+                price: 399.99,
+                stock: 10,
+                image: 'photos/shoes_3.jpg'
+            }
+        },
+        {
+            shoes: {
+                description: 'Sauvage Oxfords',
+                price: 249.99,
+                stock: 10,
+                image: 'photos/shoes_4.jpg'
+            }
+        },
+        {
+            shoes: {
+                description: 'Jenson Boot',
+                price: 299.99,
+                stock: 10,
+                image: 'photos/shoes_5.jpg'
+            }
+        },
+        {
+            shoes: {
+                description: 'English Brogue Boot',
+                price: 399.99,
+                stock: 10,
+                image: 'photos/shoes_6.jpg'
+            }
+        }
+    ]
+};
+// ****************sidebar animation*********************
+function openNav() {
+    document.getElementById('mySidenav').style.width = '250px';
+}
+
+function closeNav() {
+    document.getElementById('mySidenav').style.width = '0';
+}
+
+// ******************Jackets***************
+function oneJacket(x) {
+    return [
+        '<p class="product-description"><img src="' +
+            x.jackets.image +
+            '"/>&emsp;',
+        '<br><br><p class="product-description">Price: $' +
+            x.jackets.price +
+            '</p>',
+        '<p class="product-description">In Stock: ' + x.jackets.stock + '</p>',
+        '<p class="product-description">Type: ' +
+            x.jackets.description +
+            '</p>',
+        '<span class="product-description">Add to cart  <i class="fa fa-plus-square" aria-hidden="true"></i></span><br><br>'
+    ].join('');
+}
+
+function showJackets() {
+    var h = CLOTHING.outerwear
+        .map(function(x) {
+            return oneJacket(x);
+        })
+        .join('');
+    var html =
+        [
+            '<h2 class="shopping-header"><img class="header-company-logo" src="photos/company-logo.png">Elegant Spirit</h2><hr />'
+        ].join('') + h;
+    $('#jackets').html(html);
+}
+
+// ******************collared shirts***********************
+
+function oneCollaredShirt(x) {
+    return [
+        '<p class="product-description"><img src="' +
+            x.collared.image +
+            '"/>&emsp;',
+        '<br><br><p class="product-description">Price: $' +
+            x.collared.price +
+            '</p>',
+        '<p class="product-description">In Stock: ' + x.collared.stock + '</p>',
+        '<p class="product-description">Type: ' +
+            x.collared.description +
+            '</p>',
+        '<span class="product-description">Add to cart  <i class="fa fa-plus-square" aria-hidden="true"></i></span><br><br>'
+    ].join('');
+}
+
+function showCollaredShirts() {
+    var h = CLOTHING.collaredshirts
+        .map(function(x) {
+            return oneCollaredShirt(x);
+        })
+        .join('');
+    var html =
+        [
+            '<h2 class="shopping-header"><img class="header-company-logo" src="photos/company-logo.png">Elegant Spirit</h2><hr />'
+        ].join('') + h;
+    $('#collared-shirts').html(html);
+}
+
+// **************t shirts****************
+
+function oneTshirt(x) {
+    return [
+        '<p class="product-description"><img src="' +
+            x.tshirt.image +
+            '"/>&emsp;',
+        '<br><br><p class="product-description">Price: $' +
+            x.tshirt.price +
+            '</p>',
+        '<p class="product-description">In Stock: ' + x.tshirt.stock + '</p>',
+        '<p class="product-description">Type: ' + x.tshirt.description + '</p>',
+        '<span class="product-description">Add to cart  <i class="fa fa-plus-square" aria-hidden="true"></i></span><br><br>'
+    ].join('');
+}
+
+function showTshirts() {
+    var h = CLOTHING.tshirts
+        .map(function(x) {
+            return oneTshirt(x);
+        })
+        .join('');
+    var html =
+        [
+            '<h2 class="shopping-header"><img class="header-company-logo" src="photos/company-logo.png">Elegant Spirit</h2><hr />'
+        ].join('') + h;
+    $('#tshirts').html(html);
+}
+
+// ****************pants*****************
+
+function onePants(x) {
+    return [
+        '<p class="product-description"><img src="' +
+            x.khakis.image +
+            '"/>&emsp;',
+        '<br><br><p class="product-description">Price: $' +
+            x.khakis.price +
+            '</p>',
+        '<p class="product-description">In Stock: ' + x.khakis.stock + '</p>',
+        '<p class="product-description">Type: ' + x.khakis.description + '</p>',
+        '<span class="product-description">Add to cart  <i class="fa fa-plus-square" aria-hidden="true"></i></span><br><br>'
+    ].join('');
+}
+
+function showPants() {
+    var h = CLOTHING.pants
+        .map(function(x) {
+            return onePants(x);
+        })
+        .join('');
+    var html =
+        [
+            '<h2 class="shopping-header"><img class="header-company-logo" src="photos/company-logo.png">Elegant Spirit</h2><hr />'
+        ].join('') + h;
+    $('#pants').html(html);
+}
+
+// **************socks***************
+
+function oneSocks(x) {
+    return [
+        '<p class="product-description"><img src="' +
+            x.socks.image +
+            '"/>&emsp;',
+        '<br><br><p class="product-description">Price: $' +
+            x.socks.price +
+            '</p>',
+        '<p class="product-description">In Stock: ' + x.socks.stock + '</p>',
+        '<p class="product-description">Type: ' + x.socks.description + '</p>',
+        '<span class="product-description">Add to cart  <i class="fa fa-plus-square" aria-hidden="true"></i></span><br><br>'
+    ].join('');
+}
+
+function showSocks() {
+    var h = CLOTHING.sockfootwear
+        .map(function(x) {
+            return oneSocks(x);
+        })
+        .join('');
+    var html =
+        [
+            '<h2 class="shopping-header"><img class="header-company-logo" src="photos/company-logo.png">Elegant Spirit</h2><hr />'
+        ].join('') + h;
+    $('#socks').html(html);
+}
+
+// **************shoes***************
+
+function oneShoes(x) {
+    return [
+        '<p class="product-description"><img src="' +
+            x.shoes.image +
+            '"/>&emsp;',
+        '<br><br><p class="product-description">Price: $' +
+            x.shoes.price +
+            '</p>',
+        '<p class="product-description">In Stock: ' + x.shoes.stock + '</p>',
+        '<p class="product-description">Type: ' + x.shoes.description + '</p>',
+        '<span class="product-description">Add to cart  <i class="fa fa-plus-square" aria-hidden="true"></i></span><br><br>'
+    ].join('');
+}
+
+function showShoes() {
+    var h = CLOTHING.shoefootwear
+        .map(function(x) {
+            return oneShoes(x);
+        })
+        .join('');
+    var html =
+        [
+            '<h2 class="shopping-header"><img class="header-company-logo" src="photos/company-logo.png">Elegant Spirit</h2><hr />'
+        ].join('') + h;
+    $('#tshirts').html(html);
+}
