667b0aa6eb4dfbbe194a3fe0e8d35d520d1cf7d9
Trey Shelton <tshelton@basecampcodingacademy.org>
2018-02-22 12:37:29 -0600

    the store is now to a working place.. few minor mishaps but look to finish the rest soon

--- photos/Elegant Spirit_files/index.js
+++ photos/Elegant Spirit_files/index.js
@@ -1,72 +1,61 @@
 var TOTAL_SALE = 0;
-var STOCK = 10;
 var CLOTHING = {
     jackets: [
         {
-            type: 'jackets',
             description: 'Barn Coat',
             price: 169.99,
             stock: 10,
             image: 'photos/jacket_1.jpg'
         },
         {
-            type: 'jackets',
             description: 'Weatherbreaker Jacket',
             price: 169.99,
             stock: 10,
             image: 'photos/jacket_2.jpg'
         },
         {
-            type: 'jackets',
             description: 'Barbour Ashby Jacket',
             price: 399.99,
             stock: 10,
             image: 'photos/jacket_3.jpg'
         },
         {
-            type: 'jackets',
             description: 'Heritage Field Coat',
             price: 249.99,
             stock: 10,
             image: 'photos/jacket_4.jpg'
         },
         {
-            type: 'jackets',
             description: 'Bedale Jacket',
             price: 379.99,
             stock: 10,
             image: 'photos/jacket_5.jpg'
         },
         {
-            type: 'jackets',
             description: 'Brome Jacket',
             price: 429.99,
             stock: 10,
             image: 'photos/jacket_6.jpg'
         },
         {
-            type: 'jackets',
             description: 'Wool Driving Coat',
             price: 298.99,
             stock: 10,
             image: 'photos/jacket_7.jpg'
         },
         {
-            type: 'jackets',
             description: 'Bedale Jacket',
             price: 379.99,
             stock: 10,
             image: 'photos/jacket_8.jpg'
         },
         {
-            type: 'jackets',
             description: 'Shearling Coat',
             price: 999.99,
             stock: 10,
             image: 'photos/jacket_9.jpg'
         },
         {
-            type: 'jackets',
             description: 'Redfield Leather Jacket',
             price: 494.99,
             stock: 10,
@@ -75,70 +64,60 @@ var CLOTHING = {
     ],
     shirts: [
         {
-            type: 'shirts',
             description: 'Brushed Herringbone',
             price: 89.99,
             stock: 10,
             image: 'photos/collared_1.jpg'
         },
         {
-            type: 'shirts',
             description: 'Tartan Long-Sleeved Shirt',
             price: 89.99,
             stock: 10,
             image: 'photos/collared_2.jpg'
         },
         {
-            type: 'shirts',
             description: 'Signature Twill Long-Sleeved Shirt',
             price: 89.99,
             stock: 10,
             image: 'photos/collared_3.jpg'
         },
         {
-            type: 'shirts',
             description: 'Pure Cotton Denim Shirt',
             price: 89.99,
             stock: 10,
             image: 'photos/collared_4.jpg'
         },
         {
-            type: 'shirts',
             description: 'Hidden-Button-Down Wrinkle-Free Cotton Twill Shirt',
             price: 89.99,
             stock: 10,
             image: 'photos/collared_5.jpg'
         },
         {
-            type: 'shirts',
             description: 'Heathered Houndstooth Shirt',
             price: 89.99,
             stock: 10,
             image: 'photos/collared_6.jpg'
         },
         {
-            type: 'shirts',
             description: 'Tech Chambray Work Shirt',
             price: 89.99,
             stock: 10,
             image: 'photos/collared_7.jpg'
         },
         {
-            type: 'shirts',
             description: 'Luxury Flannel Shirt',
             price: 89.99,
             stock: 10,
             image: 'photos/collared_8.jpg'
         },
         {
-            type: 'shirts',
             description: 'Bush Shirt',
             price: 89.99,
             stock: 10,
             image: 'photos/collared_9.jpg'
         },
         {
-            type: 'shirts',
             description: 'Corded Long-Sleeved Shirt',
             price: 89.99,
             stock: 10,
@@ -147,70 +126,60 @@ var CLOTHING = {
     ],
     tshirts: [
         {
-            type: 'tshirts',
             description: 'Dri-Release Casting T-Shirt',
             price: 49.99,
             stock: 10,
             image: 'photos/tshirt_1.jpg'
         },
         {
-            type: 'tshirts',
             description: 'Dog Stamp Tee',
             price: 49.99,
             stock: 10,
             image: 'photos/tshirt_2.jpg'
         },
         {
-            type: 'tshirts',
             description: 'Trout Stamp Tee',
             price: 49.99,
             stock: 10,
             image: 'photos/tshirt_3.jpg'
         },
         {
-            type: 'tshirts',
             description: 'Hawker Henley',
             price: 79.99,
             stock: 10,
             image: 'photos/tshirt_4.jpg'
         },
         {
-            type: 'tshirts',
             description: 'Montana Morning Retro',
             price: 69.99,
             stock: 10,
             image: 'photos/tshirt_5.jpg'
         },
         {
-            type: 'tshirts',
             description: 'Pima Cotton Mockneck',
             price: 89.99,
             stock: 10,
             image: 'photos/tshirt_6.jpg'
         },
         {
-            type: 'tshirts',
             description: 'Skyline Long-Sleeved',
             price: 49.99,
             stock: 10,
             image: 'photos/tshirt_7.jpg'
         },
         {
-            type: 'tshirts',
             description: 'Rooster Long-Sleeved',
             price: 49.99,
             stock: 10,
             image: 'photos/tshirt_8.jpg'
         },
         {
-            type: 'tshirts',
             description: 'Brown Trout Dri-Release Long-Sleeved',
             price: 49.99,
             stock: 10,
             image: 'photos/tshirt_9.jpg'
         },
         {
-            type: 'tshirts',
             description: 'Fish Dri-Release Long-Sleeved',
             price: 49.99,
             stock: 10,
@@ -219,70 +188,60 @@ var CLOTHING = {
     ],
     pants: [
         {
-            type: 'pants',
             description: 'County Donegal Tweed Pants',
             price: 169.99,
             stock: 10,
             image: 'photos/pants_1.jpg'
         },
         {
-            type: 'pants',
             description: 'Ultimate Khakis-Plain',
             price: 119.99,
             stock: 10,
             image: 'photos/pants_2.jpg'
         },
         {
-            type: 'pants',
             description: 'Supercord Pants',
             price: 119.99,
             stock: 10,
             image: 'photos/pants_3.jpg'
         },
         {
-            type: 'pants',
             description: 'Comfort-Waist Merino Flannel Pants',
             price: 149.99,
             stock: 10,
             image: 'photos/pants_4.jpg'
         },
         {
-            type: 'pants',
             description: '1856 Stretch Cords',
             price: 109.99,
             stock: 10,
             image: 'photos/pants_5.jpg'
         },
         {
-            type: 'pants',
             description: 'Stretch Moleskin Pants Plain',
             price: 149.99,
             stock: 10,
             image: 'photos/pants_6.jpg'
         },
         {
-            type: 'pants',
             description: 'Wrinkle-Free Pure Cotton Chinos',
             price: 98.99,
             stock: 10,
             image: 'photos/pants_7.jpg'
         },
         {
-            type: 'pants',
             description: 'Wrinkle-Free Comfort-Waist Chinos',
             price: 109.99,
             stock: 10,
             image: 'photos/pants_8.jpg'
         },
         {
-            type: 'pants',
             description: 'Insulated Rider Pants',
             price: 79.99,
             stock: 10,
             image: 'photos/pants_9.jpg'
         },
         {
-            type: 'pants',
             description: 'All Transit Pants',
             price: 98.99,
             stock: 10,
@@ -291,28 +250,24 @@ var CLOTHING = {
     ],
     socks: [
         {
-            type: 'socks',
             description: 'Invincible Socks',
             price: 49.99,
             stock: 10,
             image: 'photos/socks_1.jpg'
         },
         {
-            type: 'socks',
             description: 'Invincible Cotton Sock',
             price: 39.99,
             stock: 10,
             image: 'photos/socks_2.jpg'
         },
         {
-            type: 'socks',
             description: 'Isle Socks',
             price: 39.99,
             stock: 10,
             image: 'photos/socks_3.jpg'
         },
         {
-            type: 'socks',
             description: 'Invincible Athletic Socks',
             price: 39.99,
             stock: 10,
@@ -321,42 +276,36 @@ var CLOTHING = {
     ],
     shoes: [
         {
-            type: 'shoes',
             description: 'Australian Shearling-Lined Slippers',
             price: 199.99,
             stock: 10,
             image: 'photos/shoes_1.jpg'
         },
         {
-            type: 'shoes',
             description: 'Gokey Shearling Lined Double-Sole Mocs',
             price: 229.99,
             stock: 10,
             image: 'photos/shoes_2.jpg'
         },
         {
-            type: 'shoes',
             description: 'Ultralight 8" Boots',
             price: 399.99,
             stock: 10,
             image: 'photos/shoes_3.jpg'
         },
         {
-            type: 'shoes',
             description: 'Sauvage Oxfords',
             price: 249.99,
             stock: 10,
             image: 'photos/shoes_4.jpg'
         },
         {
-            type: 'shoes',
             description: 'Jenson Boot',
             price: 299.99,
             stock: 10,
             image: 'photos/shoes_5.jpg'
         },
         {
-            type: 'shoes',
             description: 'English Brogue Boot',
             price: 399.99,
             stock: 10,
@@ -370,6 +319,8 @@ function capitalizeFirstLetter(string) {
 }
 
 function oneItem(x) {
+    console.log(x);
+    console.log(JSON.stringify(x));
     return [
         '<div class="col-lg-3 col-sm-6 item-div">',
         '<div class="panel panel-default"><div class="panel-body">',
@@ -384,16 +335,7 @@ function oneItem(x) {
 }
 
 function showItems(itemtype) {
-    $('#' + itemtype).removeClass('hide');
-    $('.buy-items').show();
     $('.for-sale').hide();
-    $('#cart')
-        .addClass('hide')
-        .removeClass('show');
-    $('#sell-item-form')
-        .addClass('hide')
-        .removeClass('show');
-
     var html =
         '<br><br><br><h1 id="clothing-title">' +
         capitalizeFirstLetter(itemtype) +
@@ -409,71 +351,22 @@ function showItems(itemtype) {
 }
 
 // ***********add to cart********
-function subtractStock(x) {
-    var name = x.description;
-    for (var c = 0; c < CLOTHING[x.type].length; c++) {
-        if (name == CLOTHING[x.type][c].description) {
-            x.stock -= 1;
-            CLOTHING[x.type][c] = x;
-            return true;
-        }
-    }
-}
 
 function addToCart(x) {
-    // subtractStock(x);
+    x.stock -= 1;
     TOTAL_SALE += x.price;
-    var html =
-        '<p class="navbar-cart"> Total: $' + TOTAL_SALE.toFixed(2) + '</p>';
+    var html = '<p class="navbar-cart"> Total: $' + TOTAL_SALE + '</p>';
     html += TOTAL_SALE + $('.navbar-cart').html(html);
     $('#cart').prepend(
-        '<div class="col-lg-3 col-sm-6">' +
-            '<div class="panel panel-default"><div class="panel-body">' +
-            '<p class="img-center"><img src="' +
-            x.image +
-            '"/></p>' +
-            '<p>Price: $' +
-            x.price +
-            '</p>' +
-            '<p>Type: ' +
-            x.description +
-            '</p></div></div></div>'
+        '<div class="panel panel-default"><div class="panel-body">',
+        '<p class="img-center"><img src="' + x.image + '"/></p>',
+        '<p>Price: $' + x.price + '</p>',
+        '<p>In Stock: ' + x.stock + '</p>',
+        '<p>Type: ' + x.description + '</p></div></div>'
     );
-    var clothingType = x.type;
-    subtractStock(x);
-    showItems(clothingType);
 }
 
 function showCart() {
-    $('.buy-items').hide();
-    $('#cart')
-        .removeClass('hide')
-        .show();
-}
-
-// *********sell an item*************
-function sell() {
-    $('.buy-items').hide();
-    $('#cart').hide();
-    $('.homepage').hide();
-    $('#sell-item-form')
-        .removeClass('hide')
-        .show();
-}
-
-function addtoClothing() {
-    CLOTHING[$('#type').val()].push({
-        type: $('#type').val(),
-        description: $('#description').val(),
-        price: Number($('#price').val()),
-        stock: 1,
-        image: 'photos/company-logo.png'
-    });
-    $('#type').val('');
-    $('#description').val('');
-    $('#price').val('');
-}
-
-function draw(itemtype) {
-    showItems(itemtype);
+    $('.item-div').hide();
+    $('#cart').show();
 }
